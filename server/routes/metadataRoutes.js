22fb9fd9ff629542b2a99f7e4884abdcded3210d
Michael Hiebert <michael.hiebert@oktana.com>
2025-05-01 21:03:17 -0300

    feat: add dynamic profiles

--- server/routes/metadataRoutes.js
+++ server/routes/metadataRoutes.js
@@ -3,13 +3,15 @@ import {
   upsertMetadata,
   compareMetadata,
   createSalesforceObject,
+  listProfiles,
 } from "../controllers/metadataController.js";
 import { requireAuth } from "../middleware/requireAuth.js";
 
 const router = express.Router();
 
-router.post("/upsert", requireAuth, upsertMetadata);
+router.get("/profiles", requireAuth, listProfiles);
 router.get("/compare", requireAuth, compareMetadata);
+router.post("/upsert", requireAuth, upsertMetadata);
 router.post("/create", requireAuth, createSalesforceObject);
 
 export default router;
